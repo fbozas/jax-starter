f13243e4d77d39b559680b4249831bdb2de1ce86
fbozas <fbozas@hotmail.com>
2023-09-01 22:47:02 +0300

    Added a returning Response method

--- src/main/java/gr/aueb/cf/jaxstarter/rest/HelloRestController.java
+++ src/main/java/gr/aueb/cf/jaxstarter/rest/HelloRestController.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class HelloRestController {
@@ -13,5 +14,13 @@ public class HelloRestController {
     @Produces(MediaType.TEXT_PLAIN)
     public  String getHello(){
         return "Hello Coding Factory";
+    }
+    
+    @GET
+    @Path("/get-hello-res")
+    @Produces(MediaType.TEXT_PLAIN)
+    public  Response getHelloRes(){
+//        return Response.ok("Hello Coding Factory").build();
+        return Response.status(Response.Status.OK).entity("Hello Coding Factory").build();
     }
 }
