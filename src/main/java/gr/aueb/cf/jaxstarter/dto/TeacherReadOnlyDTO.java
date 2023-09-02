d08cfb9017d306438c861e9a90131421563f4f22
fbozas <fbozas@hotmail.com>
2023-09-02 13:17:25 +0300

    Added a new controller to get all teachers

---
+++ src/main/java/gr/aueb/cf/jaxstarter/dto/TeacherReadOnlyDTO.java
@@ -0,0 +1,26 @@
+package gr.aueb.cf.jaxstarter.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class TeacherReadOnlyDTO {
+    private Long id;
+    private String ssn;
+    private String firstname;
+    private String lastname;
+
+    public TeacherReadOnlyDTO(Long id, String ssn, String firstname, String lastname) {
+        this.id = id;
+        this.ssn = ssn;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getSsn() { return ssn; }
+    public void setSsn(String ssn) { this.ssn = ssn; }
+    public String getFirstname() { return firstname; }
+    public void setFirstname(String firstname) { this.firstname = firstname; }
+    public String getLastname() { return lastname; }
+    public void setLastname(String lastname) { this.lastname = lastname; }
+}
---
+++ src/main/java/gr/aueb/cf/jaxstarter/rest/TeacherRestController.java
@@ -0,0 +1,53 @@
+package gr.aueb.cf.jaxstarter.rest;
+
+import gr.aueb.cf.jaxstarter.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.jaxstarter.model.Teacher;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Path("/teachers")
+public class TeacherRestController {
+    private final Validator validator;
+
+    public TeacherRestController(){
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        this.validator = factory.getValidator();
+    }
+
+    @GET
+    @Path("")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTeachers(){
+
+        // Assume we call service layer and get back
+        // a list of teachers
+        List<Teacher> teachers = Arrays.asList(new Teacher(1L, "123456", "Nick", "Pappas"),
+                new Teacher(2L, "123456", "Kostas", "Pegkos"),
+                new Teacher(3L, "123456", "Makis", "Pepas"),
+                new Teacher(4L, "123456", "Takis", "Bekas"));
+
+        if(teachers.size() == 0){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        List<TeacherReadOnlyDTO> teachersDto = new ArrayList<>();
+        for (Teacher teacher : teachers){
+            teachersDto.add(mapFrom(teacher));
+        }
+        return Response.status(Response.Status.OK).entity(teachersDto).build();
+    }
+
+    private TeacherReadOnlyDTO mapFrom(Teacher teacher){
+        return new TeacherReadOnlyDTO(teacher.getId(), teacher.getSsn(), teacher.getFirstname(), teacher.getLastname());
+    }
+}
